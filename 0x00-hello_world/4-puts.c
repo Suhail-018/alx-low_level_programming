b12acbb4057eb2312f9cc01bd9e6678170dc8fd6
Your Name <you@example.com>
2023-03-16 17:22:37 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,13 @@
-#!/bin/bash
-helloputs.c
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+      	puts( "\" Programming is like building a multilingual puzzle\n " );
+        return (0);
+}
