1eb80cc4e1045a0611f2cb5ba3a52eb40eb76fd5
Your Name <you@example.com>
2023-03-17 17:50:04 -0700

    l

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,17 +3,16 @@
 /* more headers goes there */
 /* betty style doc for function main goes there */
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main (void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	 char s;
-	 
-	 for (s = 'a' ; s <= 'z' ; s++) 
-		 putchar(s);
-	 putchar('\n');
-	 return (0);
+char s;
+for (s = 'a'; s <= 'z'; s++) 
+	putchar(s);
+putchar('\n');
+return (0);
 }
 
