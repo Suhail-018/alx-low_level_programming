8d879a57ed59f70365fd6804a6cbfe0a5bb82d96
Your Name <you@example.com>
2023-03-17 17:33:54 -0700

    l

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,18 @@
-juZZ
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int d;
+for (d = '0' ; d <= '9' ; d++)
+	putchar(d);
+putchar('\n');
+return (0);
+}
