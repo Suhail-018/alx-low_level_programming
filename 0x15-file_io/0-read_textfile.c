8d05430499ce956df838ce6f466b81a2bf0877ab
root <suhailyksa@gmail.com>
2023-07-10 17:06:22 -0700

    some edit for the codes

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* read_textfile- Read text file print to STDOUT.
-* @filename: text file being read
-* @letters: number of letters to be read
-* Return:wrtn actual number of bytes read and printed. 
-*/
+ * read_textfile - Read text file print to STDOUT.
+ * @filename: The text file being read.
+ * @letters: The number of letters to be read.
+ *
+ * Return: The actual number of bytes read and printed. 
+ */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 char *buff;
