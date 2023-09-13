8d1bb3e54dfbee45152bd99c9f676353618aa9c9
root <suhailyksa@gmail.com>
2023-09-13 16:50:24 -0700

    hash table data structure project

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,17 @@
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to be hashed.
+ *
+ * Return:  The calculated hash value as an unsigned long integer.
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
-
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash)+c;
+}
+return (hash);
 }
